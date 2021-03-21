05b2f58d17bec32c765116510dfe70d01a0543cb
KLIPEBOY <64357220+KLIPEBOY@users.noreply.github.com>
2021-03-21 15:09:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+	'TOKEN': 'ODIyODk3NDIwNDY3NDM3NTk5.YFY88Q.nFaKZ1PqglVwL22eICCqFAqZQSs', # Получение токена - https://prnt.sc/uofhur
+	'NAME BOT': 'BOT NAME', # Имя вашего бота
+	'ID': 'BOT ID', # Получение ID - https://prnt.sc/uofik2
+	'PREFIX': '?', # Префикс
+	'OWNER':'KLIPEBOY.py#8731',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
+	'OWNER NAME':'KLIPEBOY.py' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
+}
