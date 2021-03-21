a512a352286d7a83900dc6dd704ed21e590f9313
KLIPEBOY <64357220+KLIPEBOY@users.noreply.github.com>
2021-03-21 16:31:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 settings = {
 	'NAME BOT': 'BOT', # Имя вашего бота
 	'ID': '822897420467437599', # Получение ID - https://prnt.sc/uofik2
-	'PREFIX': '?', # Префикс
+	'PREFIX': '~', # Префикс
 	'OWNER':'KLIPEBOY.py#8731',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
 	'OWNER NAME':'KLIPEBOY.py' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
 }
